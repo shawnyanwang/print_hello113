f7c7bb2bdabfeecad057163f1b37ea133ba7fe21
shawnyanwang <shawnyanwang@gmail.com>
2016-05-17 15:46:14 -0700

    Add test project

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'The funniest joke in the world',
+    'author': 'Shawnyanwang',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'shawnyanwang@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['print_hello113'],
+    'scripts': [],
+    'name': 'print_hello113'
+}
+
+setup(**config)
