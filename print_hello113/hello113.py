e90e2b1a6dbd20957a8cbbc9dabccb1396425284
shawnyanwang <shawnyanwang@gmail.com>
2016-05-18 16:25:11 -0700

    Add a function and changed the name of py file

---
+++ print_hello113/hello113.py
@@ -0,0 +1,8 @@
+def hello113():
+    print "Hello 113"
+def npower(x,n):
+    return x**n
+if __name__ == '__main__':
+    print 'sfsg'
+    hello113()
+    print npower(10,3)
--- print_hello113/print_hello113.py
+++
@@ -1,6 +0,0 @@
-def print_hello113():
-    print "Hello 113"
-
-if __name__ == '__main__':
-    print 'sfsg'
-    print_hello113()
